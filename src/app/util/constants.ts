0b66fe66245e7abed85902efd0ff4be74acb9f57
jbi <jan.bissinger@outlook.de>
2016-05-29 20:01:32 +0200

    Add some constants

--- src/app/util/constants.ts
+++ src/app/util/constants.ts
@@ -2,3 +2,6 @@ export const host: string = "localhost";
 export const port: string = "8443";
 
 export const NOT_FOUND: string = "notFound";
+export const NOT_ALLOWED: string = "notAllowed";
+
+export const LOCALSTORAGE_AUTH: string = "auth";
