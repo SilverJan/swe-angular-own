49aca0036b574c25e9c60110ec649e1bc5cc2a40
jbi <jan.bissinger@outlook.de>
2016-05-29 20:00:51 +0200

    Rename ArticleAdministrator to ArticleAdmin

---
+++ src/app/components/article.admin.component.ts
@@ -0,0 +1,36 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {ArticleAddComponent} from './article.admin.add.component';
+import {ArticleUpdateComponent} from './article.admin.update.component';
+import {ArticleAdminService} from '../services/article.admin.service';
+/**
+ * Created by Jan on 25.05.2016.
+ */
+
+@Component({
+    template: `
+    <nav class="navbar navbar-dark bg-inverse">
+        <a class="navbar-brand" >Artikelverwaltung</a>
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" [routerLink]="['ArticleAdd']">Artikel hinzufügen</a>
+            </li>
+        </ul>
+    </nav>
+    <br>
+    <div>
+        <router-outlet></router-outlet>
+    </div>
+    `,
+    directives: [ROUTER_DIRECTIVES]
+})
+@RouteConfig([
+    {
+        path: '/articleAdd',
+        name: 'ArticleAdd',
+        component: ArticleAddComponent,
+        useAsDefault: true
+    }
+])
+export class ArticleAdminComponent {
+}
